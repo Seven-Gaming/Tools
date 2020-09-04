c2e8d7edf6391a586c302a13285e007a9324af72
MRD04 <68100377+Seven-Gaming@users.noreply.github.com>
2020-09-04 22:19:00 +0700

    Update msfnob.py

--- msfnob.py
+++ msfnob.py
@@ -116,14 +116,12 @@ def menu():
           raw_input('\n\x1b[1;91m[ \x1b[1;97mBack \x1b[1;91m]')
           bersih()
     elif pil == "4":
-        print "\033[32;1mUpdate installing...."
+        print "\033[33;1mUpdate installing...."
         os.system("rm -fr Hackfb")
         os.system("git clone https://github.com/Seven-Gaming/msfnob")
         time.sleep(1)
         os.system("cd msfnob")
         os.system("python2 msfnob.py")
-        print "Update selesai....."
-        time.sleep(2)
         menu()
     elif pil == "0":
         os.system("exit")
